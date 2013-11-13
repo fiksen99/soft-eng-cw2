da89e874921712bd33ebeb6ac9735e968bca31cb
Charlie PAUCARD <charlie@dyn1212-202.wlan.ic.ac.uk>
2013-11-13 16:08:38 +0000

    Fix a bug in MoneyFormatter and wrote unit test for the class.
    MoneyFormatter would use ',' as the decimal separator on French
    locales instead of the UK '.'. Fixed by forcing the formatter to
    use English locale.
    
    Wrote unit test to define behaviour and ensure bug does not happen
    again.

--- src/main/com/acmetelecom/MoneyFormatter.java
+++ src/main/com/acmetelecom/MoneyFormatter.java
@@ -1,10 +1,14 @@
 package com.acmetelecom;
 
 import java.math.BigDecimal;
+import java.util.Locale;
 
 class MoneyFormatter {
     public static String penceToPounds(BigDecimal pence) {
         BigDecimal pounds = pence.divide(new BigDecimal(100));
-        return String.format("%.2f", pounds.doubleValue());
+
+        // Note that we need to specify the "English" locale here because some
+        // other languages use ',' as the decimal separator.
+        return String.format(Locale.ENGLISH, "%.2f", pounds.doubleValue());
     }
 }
