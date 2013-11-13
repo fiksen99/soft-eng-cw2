4d73f8eb6132e143e388d74534b3dcfce634f17a
Jasper Pult <jp2310@imperial.ac.uk>
2013-11-13 17:30:27 +0000

    Added CallEventTest containing tests for CallEnd and CallStart.

---
+++ src/test/com/acmetelecom/CallEventTest.java
@@ -0,0 +1,30 @@
+package com.acmetelecom;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author jp2310
+ */
+public class CallEventTest {
+    private static final String CALLER = "1234";
+    private static final String CALLEE = "5678";
+    private static final long TIME = 0;
+
+    @Test
+    public void testCallStart() {
+        testCallEvent(new CallStart(CALLER, CALLEE, TIME));
+    }
+
+    @Test
+    public void testCallEnd() {
+        testCallEvent(new CallEnd(CALLER, CALLEE, TIME));
+    }
+
+    private void testCallEvent(CallEvent event) {
+        assertEquals(CALLER, event.getCaller());
+        assertEquals(CALLEE, event.getCallee());
+        assertEquals(TIME, event.time());
+    }
+
+}
