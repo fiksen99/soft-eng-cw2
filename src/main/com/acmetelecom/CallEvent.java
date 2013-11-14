5d584a657b880a5173024cc58e05e552f1760360
Charlie PAUCARD <charlie@dyn1209-166.wlan.ic.ac.uk>
2013-11-14 11:48:03 +0000

    Add equals() and hashCode() overrides for CallEvents.

--- src/main/com/acmetelecom/CallEvent.java
+++ src/main/com/acmetelecom/CallEvent.java
@@ -22,4 +22,26 @@ public abstract class CallEvent {
     public long time() {
         return time;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CallEvent)) return false;
+
+        CallEvent callEvent = (CallEvent) o;
+
+        if (time != callEvent.time) return false;
+        if (callee != null ? !callee.equals(callEvent.callee) : callEvent.callee != null) return false;
+        if (caller != null ? !caller.equals(callEvent.caller) : callEvent.caller != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = caller != null ? caller.hashCode() : 0;
+        result = 31 * result + (callee != null ? callee.hashCode() : 0);
+        result = 31 * result + (int) (time ^ (time >>> 32));
+        return result;
+    }
 }
