e5453af7014451d75546c8cccf7a199727cacd22
Charlie PAUCARD <charlie@dyn1215-9.wlan.ic.ac.uk>
2013-11-14 14:56:10 +0000

    First version of BillingSystem test.
    Does not yet test totalBill value or call costs. Need to implement a
    way to test lengthy calls without Sleeps.

--- src/test/com/acmetelecom/BillingSystemTest.java
+++ src/test/com/acmetelecom/BillingSystemTest.java
@@ -6,17 +6,33 @@ import com.acmetelecom.customer.Tariff;
 import com.acmetelecom.customer.TariffLibrary;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Captor;
+import org.mockito.MockitoAnnotations;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class BillingSystemTest {
     private BillingSystem billingSystem;
-    private static final String john = "John Smith";
-    private static final String helen = "Helen Mirren";
+    private static final String john = "1234";
+    private static final String helen = "5678";
+    private static final Customer customerJohn = new Customer("John Smith", "1234", "Standard");
+    private static final Customer customerHelen = new Customer("Helen Mirren", "5678", "Business");
+
+    private BillGenerator mockBillGenerator;
+
+    @Captor private ArgumentCaptor<Customer> customerCaptor;
+    @Captor private ArgumentCaptor<List<BillingSystem.LineItem>> callsCaptor;
+    @Captor private ArgumentCaptor<String> totalBillCaptor;
+
 
     @Test
     public void testCallInitiated() throws Exception {
@@ -32,17 +48,37 @@ public class BillingSystemTest {
 
     @Test
     public void testCreateCustomerBills() throws Exception {
+        billingSystem.callInitiated(john, helen);
+        billingSystem.callCompleted(john, helen);
+        billingSystem.createCustomerBills();
 
+        verify(mockBillGenerator, times(2)).send(customerCaptor.capture(), callsCaptor.capture(), totalBillCaptor.capture());
+
+        for (int i = 0; i < 2; i++) {
+            Customer customer = customerCaptor.getAllValues().get(i);
+            List<BillingSystem.LineItem> calls = callsCaptor.getAllValues().get(i);
+            String totalBill = totalBillCaptor.getAllValues().get(i);
+
+            assertThat(customer, anyOf(is(new CustomerMatcher(customerJohn)),
+                    is(new CustomerMatcher(customerHelen))));
+
+            if (customer.getFullName().equals("John Smith")) {
+                assertEquals("John number of calls", 1, calls.size());
+                assertEquals(helen, calls.get(0).callee());
+            }
+            else {
+                assertEquals("Helen number of calls", 0, calls.size());
+            }
+        }
     }
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
         CustomerDatabase mockDb = mock(CustomerDatabase.class);
         TariffLibrary mockTariffLib = mock(TariffLibrary.class);
-
-        // set up customers
-        Customer customerJohn = new Customer(john, "1234", "Standard");
-        Customer customerHelen = new Customer(helen, "5678", "Business");
+        mockBillGenerator = mock(BillGenerator.class);
 
         // populate customer db and mock behaviour
         List<Customer> customers = new LinkedList<Customer>();
@@ -52,10 +88,11 @@ public class BillingSystemTest {
         doReturn(customers).when(mockDb).getCustomers();
 
         // mock tariff library behaviour
-        doReturn(Tariff.Standard).when(mockTariffLib).tarriffFor(customerJohn);
-        doReturn(Tariff.Business).when(mockTariffLib).tarriffFor(customerHelen);
+        when(mockTariffLib.tarriffFor(argThat(new CustomerMatcher(customerJohn)))).thenReturn(Tariff.Standard);
+        when(mockTariffLib.tarriffFor(argThat(new CustomerMatcher(customerHelen)))).thenReturn(Tariff.Business);
 
-        billingSystem = new BillingSystem(mockDb, mockTariffLib);
+        // mock bill generator
+        billingSystem = new BillingSystem(mockDb, mockTariffLib, mockBillGenerator);
     }
 
     /**
