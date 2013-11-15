544c0bd251f1d8ebcc275b178f4eaad657ab47ef
Jasper <jp2310@imperial.ac.uk>
2013-11-15 23:04:47 +0000

    Refactored BillGenerator, HtmlPrinter and Printer to improve the way the HTML output is generated internally. Methods now don't open any tags without closing them or vice versa. Printer interface has printBill method, which generates the entire bill from top to bottom.This allows for extension by implementing the interface and giving printBill a different functionality. Old methods in HtmlPrinter still exist but are deprecated.

--- src/main/java/com/acmetelecom/Printer.java
+++ src/main/java/com/acmetelecom/Printer.java
@@ -1,10 +1,19 @@
 package com.acmetelecom;
 
+import com.acmetelecom.customer.Customer;
+
+import java.util.List;
+
 public interface Printer {
 
     void printHeading(String name, String phoneNumber, String pricePlan);
+    void printHeader(String name, String phoneNumber, String pricePlan);
 
     void printItem(String time, String callee, String duration, String cost);
+    void printItemTable(List<BillingSystem.LineItem> items);
 
     void printTotal(String total);
+    void printTotalFooter(String total);
+
+    void printBill(Customer customer, List<BillingSystem.LineItem> calls, String totalBill);
 }
