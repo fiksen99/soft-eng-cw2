0f9770c78316a02fa5c74ef6a4ca3eb2e2a8b08d
Goncalo Soares <goncalo.soares10@imperial.ac.uk>
2013-11-15 12:08:17 +0000

    Put back factories

--- src/main/java/com/acmetelecom/BillGenerator.java
+++ src/main/java/com/acmetelecom/BillGenerator.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class BillGenerator {
 
+    @Deprecated
+    public BillGenerator() {
+
+    }
+
     public void send(Customer customer, List<BillingSystem.LineItem> calls, String totalBill) {
 
         Printer printer = HtmlPrinter.getInstance();
