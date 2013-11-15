d2debd89fead2f59f70259058ff3edd8355732de
Charlie PAUCARD <charlie@dyn1215-154.wlan.ic.ac.uk>
2013-11-15 13:24:26 +0000

    Comments in MoneyFormatterTest

--- src/test/java/com/acmetelecom/MoneyFormatterTest.java
+++ src/test/java/com/acmetelecom/MoneyFormatterTest.java
@@ -6,9 +6,13 @@ import org.junit.Test;
 import java.math.BigDecimal;
 
 public class MoneyFormatterTest {
+    /**
+     * Tests conversion of pence to pounds with 2 significant figures after the decimal
+     * separator. Also tests that the decimal separator is '.'
+     * @throws Exception
+     */
     @Test
     public void testPenceToPounds() throws Exception {
-        // Pence to pounds converts with 2 significant figures after the decimal point.
         assertEquals("0.00", MoneyFormatter.penceToPounds(new BigDecimal(0)));
         assertEquals("0.50", MoneyFormatter.penceToPounds(new BigDecimal(50)));
         assertEquals("1.00", MoneyFormatter.penceToPounds(new BigDecimal(100)));
